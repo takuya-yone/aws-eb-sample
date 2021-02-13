2539ed2bf3e095c47ac9ae7f71270f33063d20f6
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-02-13 22:02:04 +0900

    up

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
-
-@app.get('/') # methodとendpointの指定
-async def hello():
+# print a nice greeting.
+def say_hello(username = "World"):
     return {"text": "hello world!"}
+
+application = Flask(__name__)
+
+application.add_url_rule('/', 'index', (lambda: say_hello()))
+
+application.add_url_rule('/<username>', 'hello', (lambda username: say_hello(username)))
+
+# run the app.
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
